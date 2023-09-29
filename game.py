a0c2205bc2dd31f09f16bb8cdef570abed00772d
NintenDuck <andrecked507@gmail.com>
2023-09-29 10:30:30 -0600

    Add: vscode gitignore & basic game loop on class

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from clear_screen import cls
 # ============================================================== #
 
 class game_status( Enum ):
+    IDLE        = 0
     STARTING    = 1
     PLAYING     = 2
     ENDED       = 3
@@ -11,12 +12,12 @@ class game_status( Enum ):
     GAMEOVER    = 5
 
 class Game:
-    def __init__(self, secret_range=5, debug=False) -> None:
-        self.debug = debug
-        self.secret_range = secret_range
-        self.secret_number = self._get_new_secret()
-        self.user_selection = self.secret_range+1
-        self.game_state = game_status.STARTING
+    def __init__(self, guess_range=5, debug=False) -> None:
+        self._debug = debug
+        self._guess_range = guess_range
+        self._secret_number = self._get_new_secret()
+        self._user_selection = self._guess_range+1
+        self._game_state = game_status.IDLE
     
     def start( self ):
         cls()
@@ -25,17 +26,47 @@ class Game:
         self._game()
     
     def _init( self ):
-        pass
+        self._game_state = game_status.STARTING
 
     def _game( self ):
-        pass
+        while self._game_state != game_status.ENDED:
+            cls()
+            if self._get_debug():
+                print("Psssst!, the secret number is", self._get_secret_number())
+                print("Your last guess was:", self._get_user_selection())
+            print("GUESS THE NUMBER BETWEEN -{0} and {0}".format(self._guess_range))
+
+            new_selection = int( input( "Your guess: " ) )
+            self._set_user_selection( new_selection )
+
+            if self._get_user_selection() == self._get_secret_number():
+                self._victory_screen()
+                self._set_game_state( game_status.ENDED )
+        
+        print("Hope to see you again soon human!!!")
+
+
     
     def _get_new_secret( self ):
-        secret_num = random.randrange( -self.secret_range, self.secret_range )
+        secret_num = random.randrange( -self._guess_range, self._guess_range )
         return secret_num
+
+    def _victory_screen( self ):
+        cls()
+        print("You Win!!!")
 
     # GETTERS
     def _get_secret_number( self ):
-        return self.secret_number
+        return self._secret_number
     def _get_user_selection( self ):
-        return self.user_selection
+        return self._user_selection
+    def _get_game_state( self ):
+        return self._game_state
+    def _get_debug( self ):
+        return self._debug
+
+    # SETTERS
+    def _set_user_selection( self, new_selection ):
+        self._user_selection = new_selection
+    def _set_game_state( self, new_state ):
+        self._game_state = new_state
