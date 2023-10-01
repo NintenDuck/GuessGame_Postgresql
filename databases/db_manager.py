1db090afa5a6a9cf5477151242562ac9f2e8820f
NintenDuck <andrecked507@gmail.com>
2023-10-01 11:46:45 -0600

    Add: db CREATE method & m_menu debug opt

--- databases/db_manager.py
+++ databases/db_manager.py
@@ -8,18 +8,18 @@ def _get_secret_data( name="", secret_file="databases/secret_keys.json" ) -> dic
         data = json.load(file)
         return data[name]
         
-def _connect_to_database():
+def _connect_to_database( file_path="databases/secret_keys.json" ):
     """Gets the connection and connection cursor as a dictionary
 
     Returns:
         dict: {'connection', 'cursor'}
     """
     connection = psycopg2.connect(
-                                dbname=_get_secret_data("db"),
-                                host=_get_secret_data("ip"),
-                                port=_get_secret_data("port"),
-                                user=_get_secret_data("user"),
-                                password=_get_secret_data("password")
+                                    dbname=_get_secret_data("db"),
+                                    host=_get_secret_data("ip"),
+                                    port=_get_secret_data("port"),
+                                    user=_get_secret_data("user"),
+                                    password=_get_secret_data("password")
                                 )
     cursor = connection.cursor()
     return {"connection":connection, "cursor": cursor}
@@ -38,3 +38,17 @@ def get_score_table() -> dict:
     cursor.close()
     connection.close()
     return score_table
+
+def add_score( new_name="none", final_score=-1 ) -> bool:
+    if new_name=="none" or final_score==-1:return
+    
+    conn_data = _connect_to_database()
+    connection = conn_data["connection"]
+    cursor = conn_data["cursor"]
+
+    cursor.execute("INSERT INTO score(\"name\", tries) VALUES ('{0}', {1})".format(new_name, final_score))
+    connection.commit()
+    
+    cursor.close()
+    connection.close()
+    return True
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from game_menu import Game
 from score_menu import show_table
-
+from databases.db_manager import add_score
 
 debug = True
 
@@ -10,15 +10,20 @@ def menu():
     print("1) PLAY.")
     print("2) SCORE TABLE.")
     print("3) EXIT.")
+    if debug:
+        print("4) DEBUG FUNCTION")
     res = int( input("Selection: ") )
 
     if res == 1:
-        new_game = Game( 5, True )
+        new_game = Game( 5, debug )
         new_game.start()
     elif res == 2:
         show_table()
+    elif res == 4 and debug:
+        print("No Method assigned!!!")
     else:
         return
 
 
-menu()
+if __name__ == "__main__":
+    menu()
