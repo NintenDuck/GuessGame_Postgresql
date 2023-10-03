2a5a818e682a7fd0c965befe4e20579d5f9d1335
NintenDuck <andrecked507@gmail.com>
2023-10-02 20:49:48 -0600

    Add: dumb stuff

--- sources/game/score_add_menu.py
+++ sources/game/score_add_menu.py
@@ -2,5 +2,6 @@ from sources.databases.db_manager import add_score
 from clear_screen import cls
 
 # TODO: CREATE AN ADD SCORE METHOD
+# Commit de relleno, lo lamento mucho
 def show_add_score_menu( final_score=-1):
     pass
